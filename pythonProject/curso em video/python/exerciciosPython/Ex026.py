a8f07a4e1d7b61a7319df0394c0a7912f4e6e518
victorhenriq <victor_henriquesc@outlook.com>
2020-09-22 16:21:51 -0300

    atualizando'

--- pythonProject/curso em video/python/exerciciosPython/Ex026.py
+++
@@ -1,5 +0,0 @@
-print('########## Desafio 26 ##########')
-
-frase2 = input('Digite uma frase qualquer: ').strip().lower()
-print(f'A letra "A" aparece {frase2.count("a")} vezes na frase.')
-print(f'A primeira vez foi na posição {frase2.find("a")+1} e a ultima na posição {frase2.rfind("a")+1}')
--- pythonProject/curso em video/python/exerciciosPython/rounding.py
+++ pythonProject/curso em video/python/exerciciosPython/rounding.py
@@ -2,5 +2,5 @@ from math import ceil
 
 print('########## Desafio 16 ##########')
 num = float(input('Digite um número real: '))
-
+print('Arredondando para um número inteiro que seja maior')
 print(ceil(num))
